2781db40d7eaa3ff2ed822aedcacaf77a177cd6a
quallsma <quallsma@msu.edu>
2017-09-08 14:30:43 -0400

    Updating github

--- src/test/java/com/pillartechnology/discountservice/ItemsTest.java
+++ src/test/java/com/pillartechnology/discountservice/ItemsTest.java
@@ -12,9 +12,9 @@ public class ItemsTest {
     @Before
     public void setUp() throws Exception {
         items = new Items();
-        items.add(new Item("Item", ItemType.Clothing, 1.0d));
-        items.add(new Item("Item", ItemType.Electronic, 1.0d));
-        items.add(new Item("Item", ItemType.Book, 1.0d));
+        items.add(new Item("Item", ItemType.Clothing, 10.0d));
+        items.add(new Item("Item", ItemType.Electronic, 10.0d));
+        items.add(new Item("Item", ItemType.Book, 10.0d));
     }
 
     @Test
@@ -24,13 +24,13 @@ public class ItemsTest {
 
     @Test
     public void getTotalPriceOfItemsBeforeDiscountIsApplied() throws Exception {
-        assertEquals(3.0d, items.getTotalPriceOfItemsBeforeDiscount(), 0);
+        assertEquals(30.0d, items.getTotalPriceOfItemsBeforeDiscount(), 0);
     }
 
     @Test
     public void getTotalPriceOfItemsAfterDiscountIsApplied() throws Exception {
         items.applyDiscountToItems(new SingleItemDiscount(DiscountType.Percentage, .5d, ItemType.Clothing));
-        System.out.println(items.getTotalPriceOfItemsAfterDiscount());
-        assertEquals(2.50d, items.getTotalPriceOfItemsAfterDiscount(), 0);
+
+        assertEquals(25.00d, items.getTotalPriceOfItemsAfterDiscount(), 0);
     }
 }
